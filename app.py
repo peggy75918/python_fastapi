be3d84ea2a83d33b5e78fab31f3117ff542d4bee
Peggy <peggy4328@gmail.com>
2023-11-27 04:02:31 +0800

    railway test

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import uvicorn
 from fastapi import FastAPI
 from homework import homework
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/homework")
 def root():
     return homework
 
 if __name__ == "__main__":
     uvicorn.run("app:app", port=5000, reload=True)
+    
+origins = [
+    'http://localhost:5001',
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=['*']
+)
